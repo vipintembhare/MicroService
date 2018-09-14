85d01c91353311427535a482de2311928f02aeae
vipin <vipinrtembhare@gmail.com>
2018-09-14 17:10:59 +0530

    config server

---
+++ microservicesWithSpringCloud/src/main/java/com/vipintembhare/microservicesWithSpringCloud/MicroservicesWithSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.vipintembhare.microservicesWithSpringCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroservicesWithSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesWithSpringCloudApplication.class, args);
+	}
+}
